c96322a2831d4fc9ec93d229bcc165bd2674064a
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-01-26 03:20:58 -0800

    fix cron template

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd /opt/app-root
 echo 'run ora2pg on nettle'
-cat cas-data-puller.py | ssh nettle.bcgov python - ora2pg/ora2pg.conf
+cat cas-data-puller.py | ssh -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA_CONF"
