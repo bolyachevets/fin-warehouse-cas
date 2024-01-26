d69d61d06e2d9c96c26ff19dee08837e4aef6ff7
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-01-26 09:15:10 -0800

    mv files

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@
 cd /opt/app-root
 echo 'run ora2pg on nettle'
 cat cas-data-puller.py | ssh -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA_CONF"
+scp -i ~/.ssh/rsa/id_rsa "$NETTLE_IP":/dsk01/warehouse/* /data
+ssh -i ~/.ssh/rsa/id_rsa "$NETTLE_IP 'rm /dsk01/warehouse/*'
