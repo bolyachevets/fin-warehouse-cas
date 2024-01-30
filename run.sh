189e90f170a67843dad7d456e7d336d8d60f0c55
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-01-30 09:34:10 -0800

    forgot to unzip

--- run.sh
+++ run.sh
@@ -8,4 +8,8 @@ cat cas-data-puller.py | ssh -v -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA
 mkdir -p /data/update
 scp -i ~/.ssh/rsa/id_rsa "$NETTLE_IP":/dsk01/warehouse/* /data/update
 ssh -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" 'rm /dsk01/warehouse/*'
+for filename in $(ls "/data/update"); do
+  echo $filename
+  gunzip "/data/update/${filename}"
+done
 python cas-upsert-formatter.py
