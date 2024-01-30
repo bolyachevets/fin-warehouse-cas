303d1ef29595d1026583fdfe907c336733dafc3a
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-01-30 00:13:42 -0800

    patch config

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 cd /opt/app-root
 echo 'run ora2pg on nettle'
-# cat cas-data-puller.py | ssh -v -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA_CONF_INSERT"
-# mkdir -p /data/insert
-# scp -i ~/.ssh/rsa/id_rsa "$NETTLE_IP":/dsk01/warehouse/* /data/insert
-# ssh -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" 'rm /dsk01/warehouse/*'
+if [ "$PATCH_CONFIG" == true ]; then
+  cat cas-patch-config.py | ssh -v -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA_CONF_UPDATE"
+fi
 cat cas-data-puller.py | ssh -v -i ~/.ssh/rsa/id_rsa "$NETTLE_IP" python - "$ORA_CONF_UPDATE"
 mkdir -p /data/update
 scp -i ~/.ssh/rsa/id_rsa "$NETTLE_IP":/dsk01/warehouse/* /data/update
